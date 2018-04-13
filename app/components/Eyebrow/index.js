9cef158d3a8d02bf6118b7a8ad33c131c6840d3d
John Son <jsson77@gmail.com>
2018-04-13 16:37:13 -0700

    add keyframes for brow raise and blink

--- app/components/Eyebrow/index.js
+++ app/components/Eyebrow/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { blink, browRaise1, browRaise2 } from './keyFrames';
 import Wrapper from './Wrapper';
 
 export const EyebrowPrimitive = styled.div.attrs({ className: 'eyebrow' })`
@@ -8,7 +9,8 @@ export const EyebrowPrimitive = styled.div.attrs({ className: 'eyebrow' })`
   height: 160px;
   border-radius: 50%;
   position: absolute;
-  left: 100px;
+  left: 50%;
+  transform: translateX(-50%);
   color: black;
   box-shadow:
     120px 240px 0 0 currentColor;
@@ -24,6 +26,7 @@ export const EyebrowPrimitive = styled.div.attrs({ className: 'eyebrow' })`
     border-radius: 0 0 0 100%;
     transform: rotate(-50deg);
     transform-origin: right 210px;
+    animation: ${browRaise2} 8s infinite alternate;
   }
   ::after {
     content: '';
@@ -36,7 +39,16 @@ export const EyebrowPrimitive = styled.div.attrs({ className: 'eyebrow' })`
     border-radius: 0 0 100% 0;
     transform: rotate(40deg);
     transform-origin: right 210px;
+    animation: ${browRaise1} 8s infinite alternate;
   }
+
+  animation: ${blink} 8s infinite;
+  ${'' /* animation-name: ${blink};
+  animation-duration: 8s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+  animation-timing-function: ease-out;
+  animation-fill-mode: forwards; */}
 `;
 
 const Eyebrow = () => (
